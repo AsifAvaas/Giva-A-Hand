e0d78f6680cffcde3d96cefd783a71fa0117f29d
Asif A Khuda <asif13.aak@gmail.com>
2025-02-04 22:32:08 +0600

    password changed

--- Server/index.js
+++ Server/index.js
@@ -4,7 +4,8 @@ import cors from 'cors';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import cookieParser from 'cookie-parser';
-
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
@@ -14,8 +15,8 @@ app.use(cookieParser());
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "precious",
-    database: "giveahand" 
+    password: process.env.MYSQL_PASSWORD,
+    database: "giveahand"
 });
 
 db.connect((err) => {
@@ -30,7 +31,7 @@ db.connect((err) => {
 app.post('/api/register', async (req, res) => {
     const { name, email, password } = req.body;
 
-    
+
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
@@ -43,7 +44,7 @@ app.post('/api/register', async (req, res) => {
             return res.status(500).json({ error: 'Database error' });
         }
 
-        
+
         const token = jwt.sign({ email }, "your_secret_key", { expiresIn: '1h' });
 
         res.status(201).json({ message: "User registered successfully", token });
